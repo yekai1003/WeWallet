4865279326c70970e4984b08634a828fac5578bb
yjjie <yjjie@163.com>
2019-01-09 11:12:17 +0800

    添加邮箱验证工具类

--- java/wewallet-common/src/main/java/net/wzero/wewallet/utils/ValidateUtils.java
+++ java/wewallet-common/src/main/java/net/wzero/wewallet/utils/ValidateUtils.java
@@ -3,9 +3,19 @@ package net.wzero.wewallet.utils;
 import net.wzero.wewallet.WalletException;
 
 public class ValidateUtils {
+	
+	public static String PHONE_REGEX = "^[1][3,4,5,6,7,8,9][0-9]{9}";
+	public static String EMAIL_REGEX = "^\\s*\\w+(?:\\.{0,1}[\\w-]+)*@[a-zA-Z0-9]+(?:[-.][a-zA-Z0-9]+)*\\.[a-zA-Z]+\\s*$";;
 
 	public static void validatePhone(String phone) {
-		if(!phone.matches("^[1][3,4,5,6,7,8,9][0-9]{9}")) 
+		if(!phone.matches(PHONE_REGEX)) 
 			throw new WalletException("phone_error", "手机号格式不正确");
 	}
+	
+	public static Boolean validateEmail(String eamil) {
+		if(eamil.matches(EMAIL_REGEX))
+			return true;
+		return false;
+	}
+	
 }
